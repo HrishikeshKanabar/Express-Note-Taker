b148b6f15f7de7a2ece92f15319de53d1a9db337
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-03 12:16:04 -0400

    Port resolution heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const exp = require("express");
 
 // Start
 const app = exp();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(exp.urlencoded({ extended: true }));
