e26cf783535de4bc9d11755adf62ea56711ea1ea
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-03 12:26:26 -0400

    Test heroku link

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(exp.urlencoded({ extended: true }));
 app.use(exp.json());
 
 app.get('/api', (req, res) => {
-    res.sendFile(path.join(__dirname + "/../public/index.html"));
+    res.send("Hello")
   });
 
 app.listen(PORT, () => {
