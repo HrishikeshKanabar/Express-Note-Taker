f3c0820c43fb06a61ebc93a2c6e8a64c0b508d23
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-03 11:58:18 -0400

    Server intial

---
+++ server.js
@@ -0,0 +1,18 @@
+// Express JS dependency
+const exp = require("express");
+
+// Start
+const app = exp();
+const PORT = 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(exp.urlencoded({ extended: true }));
+app.use(exp.json());
+
+app.get('/api/animals', (req, res) => {
+    res.send('Hello!');
+  });
+
+app.listen(PORT, () => {
+    console.log('API server Started on port 3001 !');
+});
