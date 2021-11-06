6ab10f5487653474827f77cbc930a64f0c65d539
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-05 21:39:06 -0400

    Bonus delete functionality and formating
    Task achieved:
    WHEN I click on an existing note in the list in the left-hand column
    THEN that note appears in the right-hand column
    WHEN I click on the Write icon in the navigation at the top of the page
    THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Express JS dependency
 const exp = require("express");
+const { copyFileSync } = require("fs");
 const path = require("path");
 const dbData = require("./db/db.json");
 
@@ -10,30 +11,25 @@ const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(exp.urlencoded({ extended: true }));
 app.use(exp.json());
-app.use(exp.static(__dirname + '/public'));
+app.use(exp.static(__dirname + "/public"));
 
 //  #####  Routes for HTML file  ########
 
-
 // Route get to request for landing page
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
-  });
-
+});
 
 // Route get to  request for notes pages
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/notes.html"));
 });
 
-
-
-//  #####  Routes for API ctions or events  ########
-
+//  #####  Routes for API actions or events  ########
 
 // Route get to display all notes in Json
 app.get("/api/notes", (req, res) => {
-    res.json(dbData);
+  res.json(dbData);
 });
 
 // Route post to save notes
@@ -44,17 +40,16 @@ app.post("/api/notes", (req, res) => {
   res.json(dbData);
 });
 
-
-
-
-
-
-
-
-
+app.delete("/api/notes/:id", (req, res) => {
+  let noteTitleId = req.params.id;
+  for (let data in dbData) {
+    if (dbData[data].title == noteTitleId) {
+      dbData.splice(data, 1);
+    }
+  }
+});
 
 // port test
 app.listen(PORT, () => {
-    console.log('API server Started on port 3001 !');
+  console.log("API server Started on port 3001 !");
 });
-
